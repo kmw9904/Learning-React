8d7a3f9e000a697b3da05132c5e5d941471f90ab
kmw <skqjahjakskxl@gmail.com>
2024-12-23 16:37:25 +0900

    라이프사이클 & Effect Hook

--- vite-project/src/AppTodo.jsx
+++ vite-project/src/AppTodo.jsx
@@ -2,8 +2,10 @@ import "./App.css";
 import TodoList from "./components/todo/TodoList";
 import AddTodo from "./components/todo/AddTodo";
 import { TodoProvider } from "./context/TodoContext";
+import { useEffect } from "react";
 
 function AppTodo() {
+
   return (
     <TodoProvider>
       <h2>할일 목록</h2>
