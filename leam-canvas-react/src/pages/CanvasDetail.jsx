e20180c55eb3a4d45dca4883d9062cd02b4c899f
kmw <skqjahjakskxl@gmail.com>
2025-01-09 03:08:32 +0900

    린캔버스 UI 만들기 (with Tailwind CSS)

--- leam-canvas-react/src/pages/CanvasDetail.jsx
+++ leam-canvas-react/src/pages/CanvasDetail.jsx
@@ -1,15 +1,10 @@
-import { useLocation, useParams, useSearchParams } from 'react-router-dom';
-
+import CanvasTitle from '../components/CanvasTitle';
+import LeanCanvas from '../components/LeanCanvas';
 function CanvasDetail() {
-  const { id } = useParams();
-  const [searchParams] = useSearchParams();
-  const location = useLocation()
   return (
     <div>
-      CanvasDetaiL!
-      <p>id: {id}</p>
-      <p>keyword: {searchParams.get('keyword')}</p>
-      <p>hash : {location.hash}</p>
+      <CanvasTitle />
+      <LeanCanvas />
     </div>
   );
 }
